5c97716e87555903c1732dd7772960b4d480b837
R O M U L L O <developermarsh@gmail.com>
2019-04-27 08:19:05 -0300

    Clean script gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
+// Consts
 const gulp = require('gulp'),
       sass = require('gulp-sass');
 
+// Module compiler
 sass.compiler = require('node-sass');
 
+// Default
 gulp.task('default', watch);
 
-gulp.task('sass', compileSass);
-
+// Compiler SASS
 function compileSass(){
     return gulp
-    .src("src/scss/**/*.scss")
-    .pipe(sass({outputStyle: ''}).on("error", sass.logError))
-    .pipe(gulp.dest("src/css/"));
+    .src("src/scss/**/*.scss").pipe(sass({outputStyle: 'compact'}).on("error", sass.logError)).pipe(gulp.dest("src/css/"));
 }
 
+// WATCH
 function watch(){
     gulp.watch("src/scss/**/*.scss", compileSass)
     console.log("SASS Runing...")
